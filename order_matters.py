88c77c61980b769017965591dc45391d4da7f56f
Salam Waddah <salamwaddah@gmail.com>
2019-06-14 22:50:14 +0400

    order matters

---
+++ order_matters.py
@@ -0,0 +1,14 @@
+import turtle
+
+t = turtle.Turtle()
+t.color('red')
+t.forward(100)
+t.right(90)
+t.color('orange')
+t.forward(100)
+t.right(90)
+t.color('yellow')
+t.forward(100)
+t.right(90)
+t.color('green')
+t.forward(100)
