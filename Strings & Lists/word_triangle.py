d0476b6dacafd1fa45ac959e0501815299e5004e
Salam Waddah <salamwaddah@gmail.com>
2019-06-22 21:42:50 +0400

    word triangle

---
+++ Strings & Lists/word_triangle.py
@@ -0,0 +1,17 @@
+# d
+# de
+# def
+# defi
+# defin
+# defini
+# definit
+# definite
+# definitel
+# definitely
+
+word = "definitely"
+length = len(word)
+
+for n in range(length):
+    # Add your code here.
+    print(word[:n + 1])
